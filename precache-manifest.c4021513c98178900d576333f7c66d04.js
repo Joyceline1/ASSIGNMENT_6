a1bf8cfe9dd7af522b719fac94df16003556abd9
Joyceline1 <sweetjoy.ja59@gmail.com>
2020-09-10 19:50:12 -0700

    Updates

---
+++ precache-manifest.c4021513c98178900d576333f7c66d04.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b77e4e3db359c3e6f17b5e7e72a1ecf",
+    "url": "/ASSIGNMENT_6/index.html"
+  },
+  {
+    "revision": "620ce2ece1e82572b0da",
+    "url": "/ASSIGNMENT_6/static/css/main.0c6b3fb1.chunk.css"
+  },
+  {
+    "revision": "d2e373c254585c9be00e",
+    "url": "/ASSIGNMENT_6/static/js/2.a7f2996d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ASSIGNMENT_6/static/js/2.a7f2996d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "620ce2ece1e82572b0da",
+    "url": "/ASSIGNMENT_6/static/js/main.f3cb5ab3.chunk.js"
+  },
+  {
+    "revision": "db11158be6f0d92c232c",
+    "url": "/ASSIGNMENT_6/static/js/runtime-main.b12a42a5.js"
+  },
+  {
+    "revision": "df53022ea211813bc9591239d4f321fc",
+    "url": "/ASSIGNMENT_6/static/media/logo.df53022e.PNG"
+  }
+]);
